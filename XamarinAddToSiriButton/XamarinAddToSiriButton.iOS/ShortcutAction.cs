71bd98c472425af337c913c82b09eed73c633f41
StartBits <85449471+startbits@users.noreply.github.com>
2022-02-12 13:47:40 +0900

    Add siri shortcut button.

---
+++ XamarinAddToSiriButton/XamarinAddToSiriButton.iOS/ShortcutAction.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+namespace XamarinAddToSiriButton.iOS
+{
+    internal static class ShortcutAction
+    {
+        private static string ActionName => $"{NSBundle.MainBundle.BundleIdentifier}.ShortcutAction";
+
+        internal static NSUserActivity CreateShortcut()
+        {
+            var userActivity = new NSUserActivity(ActionName);
+            userActivity.PersistentIdentifier = ActionName;
+            userActivity.Title = "Shortcut";
+            userActivity.SuggestedInvocationPhrase = "Open Sesame";
+            return userActivity;
+        }
+    }
+}
---
+++ XamarinAddToSiriButton/XamarinAddToSiriButton.iOS/SiriShortcutPageRenderer.cs
@@ -0,0 +1,84 @@
+﻿using Foundation;
+using Intents;
+using IntentsUI;
+using ObjCRuntime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(XamarinAddToSiriButton.MainPage), typeof(XamarinAddToSiriButton.iOS.SiriShortcutPageRenderer))]
+namespace XamarinAddToSiriButton.iOS
+{
+    partial class SiriShortcutPageRenderer : PageRenderer
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var siriButton = new INUIAddVoiceShortcutButton(INUIAddVoiceShortcutButtonStyle.Automatic);
+            siriButton.Shortcut = new INShortcut(ShortcutAction.CreateShortcut());
+            siriButton.Delegate = this;
+            siriButton.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            this.View.AddSubview(siriButton);
+            this.View.CenterXAnchor.ConstraintEqualTo(siriButton.CenterXAnchor).Active = true;
+            this.View.CenterYAnchor.ConstraintEqualTo(siriButton.CenterYAnchor).Active = true;
+        }
+    }
+
+    partial class SiriShortcutPageRenderer : IINUIAddVoiceShortcutButtonDelegate
+    {
+        public void PresentAddVoiceShortcut(INUIAddVoiceShortcutViewController addVoiceShortcutViewController, INUIAddVoiceShortcutButton addVoiceShortcutButton)
+        {
+            addVoiceShortcutViewController.Delegate = this;
+            addVoiceShortcutViewController.PresentationController.Delegate = this;
+            this.PresentViewController(addVoiceShortcutViewController, true, null);
+        }
+
+        public void PresentEditVoiceShortcut(INUIEditVoiceShortcutViewController editVoiceShortcutViewController, INUIAddVoiceShortcutButton addVoiceShortcutButton)
+        {
+            editVoiceShortcutViewController.Delegate = this;
+            editVoiceShortcutViewController.PresentationController.Delegate = this;
+            this.PresentViewController(editVoiceShortcutViewController, true, null);
+        }
+    }
+
+    partial class SiriShortcutPageRenderer : IINUIAddVoiceShortcutViewControllerDelegate
+    {
+        public void DidCancel(INUIAddVoiceShortcutViewController controller)
+        {
+            controller.DismissViewController(true, null);
+        }
+
+        public void DidFinish(INUIAddVoiceShortcutViewController controller, INVoiceShortcut voiceShortcut, NSError error)
+        {
+            controller.DismissViewController(true, null);
+        }
+    }
+
+    partial class SiriShortcutPageRenderer : IINUIEditVoiceShortcutViewControllerDelegate
+    {
+        public void DidCancel(INUIEditVoiceShortcutViewController controller)
+        {
+            controller.DismissViewController(true, null);
+        }
+
+        public void DidDelete(INUIEditVoiceShortcutViewController controller, NSUuid deletedVoiceShortcutIdentifier)
+        {
+            controller.DismissViewController(true, null);
+        }
+
+        public void DidUpdate(INUIEditVoiceShortcutViewController controller, INVoiceShortcut voiceShortcut, NSError error)
+        {
+            controller.DismissViewController(true, null);
+        }
+    }
+
+    partial class SiriShortcutPageRenderer : IUIAdaptivePresentationControllerDelegate
+    {
+    }
+}
